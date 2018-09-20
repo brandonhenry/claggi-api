89f41428f4677b4ab1d808c599f36419ed720ef4
itsbhenry <itsbhenry@gmail.com>
2018-09-20 10:15:04 -0500

    added methods for setting and validating the user's password

--- config/index.js
+++ config/index.js
@@ -1,9 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = {
+  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+};
--- models/User.js
+++ models/User.js
@@ -1,11 +1,37 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var secret = require('../config').secret;
+
 
 var UserSchema = new mongoose.Schema({
-	username: String,
-	email: String,
+	username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+	email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
 	ebayUsername: String,
 	ebayToken: String,
-	hash: String
+	hash: String,
+	salt: String
 }, {timestamps: true});
 
+UserSchema.plugin(uniqueValidator, {message: 'is already taken'});
+
+UserSchema.methods.setPassword = function(password){
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
+
+};
+
+UserSchema.methods.validPassword = function(password){
+	var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+	return this.hash === hash;
+};
+
+UserSchema.methods.setEbayToken = function(){
+
+};
+
+UserSchema.methods.getEbayToken = function(){
+	return this.ebayToken;
+}
 mongoose.model('User', UserSchema);
