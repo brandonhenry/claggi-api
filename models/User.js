13b2ca405d34df7e3c37536dce9744fa9149b796
itsbhenry <itsbhenry@gmail.com>
2018-09-20 09:30:49 -0500

    updated app.js file for deploying

--- models/User.js
+++ models/User.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
 	username: String,
-	email: String
+	email: String,
+	ebayUsername: String,
+	ebayToken: String,
+	hash: String
 }, {timestamps: true});
 
 mongoose.model('User', UserSchema);
