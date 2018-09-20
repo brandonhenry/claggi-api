201991bfbce098011e3bf024d7492a9051e74264
itsbhenry <itsbhenry@gmail.com>
2018-09-20 11:47:23 -0500

    Added user route

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,4 +4,18 @@ var router = express.Router();
 router.use('/listings', require('.listings'));
 router.use('/', require('./users'));
 
+router.use(function(err, req, res, next){
+  if (err.name === 'ValidationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors, key){
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {})
+    });
+  }
+
+  return next(err);
+});
+
 module.exports = router;
