cc0b4d81e91fcc2ff7bdf587d75e8868d289bde6
Brandon <itsbhenry@gmail.com>
2018-09-19 23:07:18 -0500

    added credentials to separate file

---
+++ config/credentials.js
@@ -0,0 +1,8 @@
+var ebaytoken = 'v^1.1%23i^1%23p^3%23f^0%23r^1%23I^3%23t^Ul41XzE6N0Q5MDM2QzdCNjQ3RjJDODlFRjM1ODRDNEQ5MzhCMTNfMl8xI0VeMjYw';
+
+var appID = 'BrandonH-SkuGrid-PRD-066850dff-54313674';
+var devID = '986cfb64-ae6a-4a21-95fb-f21a670ae53f';
+var cert = 'PRD-66850dfff6e9-feec-4725-8aed-13b2';
+
+var eBayCredentials = {appID, devID, cert, ebaytoken};
+module.exports = eBayCredentials;
--- routes/api/listings.js
+++ routes/api/listings.js
@@ -2,20 +2,12 @@ var router = require('express').Router();
 var mongoose = require('mongoose');
 var Listing = mongoose.model('Listing');
 var request = require('request');
-
-function getSessionID(){
-
-    var appID = 'BrandonH-SkuGrid-PRD-066850dff-54313674';
-    var devID = '986cfb64-ae6a-4a21-95fb-f21a670ae53f';
-    var cert = 'PRD-66850dfff6e9-feec-4725-8aed-13b2';
-
-    
-}
+var credentials = require('../config/credentials.js');
 
 var options = {
     url: 'https://api.ebay.com/sell/inventory/v1/inventory_item',
     headers: {
-        'Authorization': 'Bearer <ebayToken>',
+        'Authorization': `Bearer <${credentials.ebaytoken}>`,
         'Content-Language': 'en-US',
         'X-EBAY-C-MARKETPLACE-ID': 'EBAY_US'
     }
