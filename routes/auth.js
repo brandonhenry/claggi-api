fda55ea981ddf65a8237863b8b3aa9bf614c48d6
itsbhenry <itsbhenry@gmail.com>
2018-09-20 10:43:40 -0500

    removed unused users route and added passport local strategy

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+var jwt = require('express-jwt');
+var secret = require('../config').secret;
+
+function getTokenFromHeader(req){
+  if (req.headers.quthorization && req.headers.authorization.split(' ')[0] === 'Token'){
+    return req.headers.authorization.split(' ')[1];
+  }
+
+  return null;
+};
