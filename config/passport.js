ad209f420af55dee7e2ab914dcb8da06de76c811
Brandon <itsbhenry@gmail.com>
2018-09-21 06:55:12 -0500

    update app.js

--- config/passport.js
+++ config/passport.js
@@ -3,15 +3,15 @@ var LocalStrategy = require('passport').Strategy;
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
-passport.use(new LocalStrategy({
-  usernameField: 'user[email]',
-  passwordField: 'user[password]'
-}, function(email, password, done){
-  User.findOne({email: email}).then(function(user){
-    if (!user || !user.validPassword(password)){
-      return done(null, false, {errors: {'email or password': 'is invalid'}})
-    }
+passport.use('LocalStrategy', new LocalStrategy({
+    usernameField: 'user[email]',
+    passwordField: 'user[password]'
+}, function (email, password, done) {
+    User.findOne({email: email}).then(function (user) {
+        if (!user || !user.validPassword(password)) {
+            return done(null, false, {errors: {'email or password': 'is invalid'}})
+        }
 
-    return done(null, user);
-  }).catch(done);
+        return done(null, user);
+    }).catch(done);
 }));
